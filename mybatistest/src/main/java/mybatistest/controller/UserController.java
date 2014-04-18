382b03147941669df2c3d99cfa39f60e84e0ce5c
cdcongcong <cdcongcong@163.com>
2014-04-18 22:51:38 +0800

    1

--- mybatistest/src/main/java/mybatistest/controller/UserController.java
+++ mybatistest/src/main/java/mybatistest/controller/UserController.java
@@ -1,7 +1,11 @@
 package mybatistest.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import mybatistest.entity.User;
 import mybatistest.service.UserService;
+import mybatistest.utils.DaoHelper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,7 +22,7 @@ public class UserController extends BaseController  {
 	
 //	@RequestMapping("login")
 	@RequestMapping(value="/login",   
-	            method=RequestMethod.GET,produces={"application/xml", "application/json"}) 	
+	            method=RequestMethod.GET,produces={"text/html", "application/xml", "application/json"}) 	
 	// 子request请求url，拼接后等价于/user/login
 	public ModelAndView Login(String userName, String password) {
 		System.out.println("===============");
@@ -28,11 +32,21 @@ public class UserController extends BaseController  {
         ModelAndView mv = new ModelAndView();   
         //添加模型数据 可以是任意的POJO对象   
 //        mv.addObject("message", userName + "! Hello World!");  
-        User user = userService.getByID("1");
+//        User user = userService.getByID("1");
 //        mv.addObject("message", user.getUserName() + "! Hello World!");
         //设置逻辑视图名，视图解析器会根据该名字解析到具体的视图页面   
 //        mv.setViewName("hello");  // 逻辑视图名
+        User user = null;
+        user = new User();
+        user.setUserID(DaoHelper.getUUID());
+        user.setUserNumber("001");
+        user.setUserName("李四");
+        user.setRemark("备注");
         mv.addObject(user);
+        Map<String, User> mp = new HashMap<>();
+        mp.put("message", user);
+        mv.addAllObjects(mp);
+        mv.setViewName("hello");
         return mv;           
 	}
 }
