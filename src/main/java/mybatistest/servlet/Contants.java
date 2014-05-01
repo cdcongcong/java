b4f9c3bd63589dea7269f7756e451785db9fcafb
cdcongcong <cecongcong@163.com>
2014-05-01 20:10:04 +0800

    q

---
+++ src/main/java/mybatistest/servlet/Contants.java
@@ -0,0 +1,10 @@
+package mybatistest.servlet;
+
+public class Contants {
+
+	public static final String USER_SESSION_ID = "userSessionID";
+	public static final String USER_ID = "userID";
+	public static final String USER_NAME = "userName";
+	
+	
+}
